84f49f85252dcd6e8e04182b3bb88f7ddcb0d787
saran2hema <51939545+saran2hema@users.noreply.github.com>
2024-06-08 06:16:59 -0500

    Update LoginPage.java

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,10 +1,16 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import utils.CommonUtils;
 
 public class LoginPage extends BasePage {
 
